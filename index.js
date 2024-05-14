6f385ce1c12ce961d0bfa5234d9556424dffa435
nasrashvili <87001814+nasrashvili@users.noreply.github.com>
2024-05-13 20:34:38 -0400

    Add files via upload
    რაც არი ეს არი

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let password = prompt('პაროლი')
+if ( password  == '20002001') {
+  prompt("სწორია")
+ 
+} else {
+ prompt('მოინდომე')
+}
+let lovefor = prompt('წარმატების საიდუმლო')
+if ( lovefor == 'სიყვარულია') {
+  prompt("სწორია")
+ 
+} else {
+ prompt('მე მჯერა შენი ... განაგრძე')
+}
+
+var   number = prompt('Luka')
+if (  number == ' nasrashvili') {
+  prompt("+1 929 715 45 45 ")
+ 
+} else {
+ prompt('site by : Nasrashvili luka')
+}
